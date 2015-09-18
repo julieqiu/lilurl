dffc9fa95fb09ad62b96cdd201ca492e7caf1885
Julie Qiu <julieyeqiu@gmail.com>
2015-09-18 11:40:13 -0400

    removed host

--- lilurl/views.py
+++ lilurl/views.py
@@ -19,8 +19,8 @@ def postdetails(request):
     if request.method == 'GET':
         return HttpResponse('post details get')
     if request.method == 'POST':
-        host = request.get_host()
-        host = 'http://'+host
+        #host = request.get_host()
+        #host = 'http://'+host
         url = request.POST.get('link')
         if (url is not None) and (url!=""):
             key = ''.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(8))
@@ -28,21 +28,10 @@ def postdetails(request):
             q.save()
             #global link_data
             #link_data.append(q)
-            data = json.dumps([{'link':url, 'code': host+'/'+key}])
+            #data = json.dumps([{'link':url, 'code': host+'/'+key}])
+            data = json.dumps([{'link':url, 'code': key}])
             return HttpResponse(data)
 
-def linkdata(request):
-    json_string = serializers.serialize("json", link_data)
-    return HttpResponse(json_string)
-"""
-    link_data = json.loads(json_string)
-    for d in link_data:
-        del d['pk']
-        del d['model']
-    link_data = json.dumps(link_data)
-    print link_data
-    return HttpResponse(link_data)
-"""
 def redir(request, shortener_code):
     url = get_object_or_404(Url, shortener_code=shortener_code)
     link = url.redirect_url
