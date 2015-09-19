6a31443884cc0c295110ba811bab1ecfabc4f52e
Julie Qiu <julieyeqiu@gmail.com>
2015-09-19 18:38:04 -0400

    hsh

--- lilurl/hash_function.py
+++ lilurl/hash_function.py
@@ -1,19 +1,10 @@
-#!/usr/bin/env python
-
-''' USE MD5 HASHING FUCNTION TO CREATE A HASH FOR THE URL.
-    ENCODES IN BASE64. '''
-
-import base64
-import hashlib
-import random
-import string
+import base64, hashlib, random, string
 
 def id_generator(size=6, chars=string.ascii_uppercase + string.digits):
     return ''.join(random.choice(chars) for _ in range(size))
 
-
 def hashFunction(url, SIZEURL=4):
-    url = url + id_generator()
+    url = (url + id_generator()).encode()
     shorter = base64.b64encode(hashlib.md5(url).digest()[-SIZEURL:])
     sanitized = shorter.replace('=','').replace('/','_').replace('+','_')
     return sanitized
