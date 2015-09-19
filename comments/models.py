faa7565a5573b62644c71af27812201b72879cd3
Julie Qiu <julieyeqiu@gmail.com>
2015-09-19 18:46:48 -0400

    cleaned

--- comments/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-
-# Create your models here.
-class Url(models.Model):
-    shortener_code = models.CharField(max_length=8)
-    redirect_url = models.CharField(max_length=200)
-
-
