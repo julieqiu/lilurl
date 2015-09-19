1f5ddfb623576211082f9de4e0f562b18ebafc7c
Julie Qiu <julieyeqiu@gmail.com>
2015-09-19 18:28:58 -0400

    lilurl hash

--- lilurl/views.py
+++ lilurl/views.py
@@ -8,25 +8,25 @@ from .models import Url
 import random
 import string
 import json
+from hash_function import hashFunction
 
 @ensure_csrf_cookie
 def index(request):
     return render(request, 'lilurl/index.html')
 
 def postdetails(request):
-    if request.method == 'GET':
-        return HttpResponse('post details get')
     if request.method == 'POST':
         host = request.get_host()
         host = 'http://'+host+'/'
         url = request.POST.get('link')
         if (url is not None) and (url!=""):
-            key = ''.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(8))
+            key = hashFunction(url)
             q = Url(shortener_code=key, redirect_url=url)
             q.save()
             data = json.dumps([{'link':url, 'code': host+key}])
             print data
             return HttpResponse(data)
+    return HttpResponse('post details get')
 
 def redir(request, shortener_code):
     url = get_object_or_404(Url, shortener_code=shortener_code)
