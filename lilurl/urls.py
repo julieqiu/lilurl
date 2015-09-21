d1a1053645342bfea763832718e8e3f6e1b3a8ee
Julie Qiu <julieyeqiu@gmail.com>
2015-09-20 20:22:54 -0400

    favicon

--- lilurl/urls.py
+++ lilurl/urls.py
@@ -6,10 +6,6 @@ urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^postdetails/', views.postdetails, name='postdetails'),
     url(r'^lilurls/', views.lilurls, name='lilurls'),
-    #url(r'^linkdata/', views.linkdata, name='linkdata'),
-    #url(r'^base/', views.base, name='base'),
     url(r'^(?P<shortener_code>\w+)/$', views.redir, 
         name='redir'),
-    #url(r'^$', TemplateView.as_view(template_name='index.html'
-    #url(r'^api/(?P<shortener_code>\w+)$', views.url_detail),
 ]
--- mysite/urls.py
+++ mysite/urls.py
@@ -16,9 +16,10 @@ Including another URLconf
 from django.conf.urls import include, url
 from django.contrib import admin
 from django.views.generic import RedirectView
+from django.contrib.staticfiles.storage import staticfiles_storage
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
     url(r'^', include('lilurl.urls', namespace="lilurl")),
-    url(r'^favicon\.ico$', RedirectView.as_view(url='/static/images/favicon.png'))
+    url(r'^favicon.ico$', RedirectView.as_view(url=staticfiles_storage.url('img/favicon.png'), permanent=False), name="favicon")
 ]
